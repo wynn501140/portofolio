4010716774294c528d00f7541a1725fdfeb36097
Wynn <ibnsyech04@gmail.com>
2026-09-08 12:24:10 +0700

    update footer

--- components/Footer.js
+++ components/Footer.js
@@ -12,19 +12,19 @@ export default function Footer() {
 
           <div className="footer-contacts">
             <div className="contact-row">
-              <span className="ico">Email:</span> ibnsyech04@gmail.com
+              <span className="ico"></span> ibnsyech04@gmail.com
             </div>
             <div className="contact-row">
-              <span className="ico">LinkedIn:</span> linkedin.com/in/ibn-syech-b35018397
+              <span className="ico"></span> linkedin.com/in/ibn-syech-b35018397
             </div>
             <a href="https://github.com/wynn501140" target="_blank" rel="noopener noreferrer" className="contact-row">
-              <span className="ico">Github:</span> github.com/wynn501140
+              <span className="ico"></span> github.com/wynn501140
             </a>
             <a href="https://instagram.com/ibn.syahab._" target="_blank" rel="noopener noreferrer" className="contact-row">
-              <span className="ico">Instagram:</span> @ibn.syahab._
+              <span className="ico"></span> @ibn.syahab._
             </a>
             <a href="https://t.me/Mywynn" target="_blank" rel="noopener noreferrer" className="contact-row">
-              <span className="ico">Telegram:</span>  @Mywynn 
+              <span className="ico"></span>  @Mywynn 
             </a>
           </div>
         </div>
